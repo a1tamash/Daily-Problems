1970996169aa38ba39d0c1e5825df26c26b44aee
Altamash Malik <malikaltamashhh@gmail.com>
2026-02-02 01:18:54 +0530

    code

---
+++ Problems/A_Mainak_and_Array.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+
+public class A_Mainak_and_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt() ;
+        while( t-- > 0 ){
+            solve(sc) ; 
+        }
+    }
+
+    public static void solve(Scanner sc){
+        int n = sc.nextInt(); 
+    }
+}
